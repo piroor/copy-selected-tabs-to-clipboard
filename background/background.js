cf51f725b45f45aff1d9a6033ac4ba4a357f4d2e
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-04-24 10:30:20 +0900

    Add description why the option is required

--- background/background.js
+++ background/background.js
@@ -112,7 +112,7 @@ async function onShortcutCommand(command) {
       const formats = configs.copyToClipboardFormats;
       const result = await RichConfirm.showInPopup(activeTab.windowId, {
         modal:   true,
-        url:     '/resources/blank.html',
+        url:     '/resources/blank.html', // required on Firefox ESR68
         title:   browser.i18n.getMessage('command_copySelectedTabs_title'),
         message: browser.i18n.getMessage('command_copySelectedTabs_message'),
         buttons: formats.map(format => format.label)
