9ecc2f0b0b1d4b49b96b6e8fc48b8b8831add585
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2022-10-04 14:29:28 +0900

    Add notes about each testcase

--- test/test-placeholder-parser.js
+++ test/test-placeholder-parser.js
@@ -11,18 +11,26 @@ const { is /*, ok, ng*/ } = assert;
 export function testProcessorCalls() {
   const input = `
     %unquoted(1st, 2nd, 3rd)%
+     => for backward compatibility of TST_INDENT
     %single-quoted('1st', '2nd', '3rd')%
     %double-quoted("1st", "2nd", "3rd")%
     %back-quoted(\`1st\`, \`2nd\`, \`3rd\`)%
     %multiple-parens(1st)(2nd)(3rd)%
+     => for backward compatibility
     %mixed-quoted(1"s"'t', '2'n"d", "3"'r'd)%
+     => sequencial tokens should be treated as a connected string
     %whitespace(   )%
+     => for backward compatibility of TST_INDENT
     %escaped-terminator\\%%
     %escaped-open-paren\\(%
     %escaped-close-paren(\\))%
     %escaped-close-quotations("\\"", '\\'')%
     %no-need-to-escape-character(\\a, "\\a\\'", '\\a\\"')%
+     => backslash preceding to a character no need to be escaped are treated
+        as a backslash character - this is for convenience about cases like
+        meta characters in regular expressions
     %escaped-arg("^\\w+//[^#]+/([\\d]+)+$")%
+     => thus we just need to write "\w" as "\w" instead of too complex "\\w".
     %"quoted-name()%"%
     %parent(%1st-child%, "%2nd-child%", '%3rd-child(a, b, c)%')%
     %grand-parent("%parent2(%1st-child2%, "%2nd-child2%", '%3rd-child2(a, b, c)%')%")%
